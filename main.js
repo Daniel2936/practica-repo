2e76d5d057a78f0af99aea648e97fc8bfb633103
StevenA1402 <gonzalezelvert@gmail.com>
2023-02-27 12:31:36 -0500

    cambios

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-let nombre = document.querySelector("#nombre")
-let edad = document.querySelector("#edad")
-let bNombre =document.querySelector("#bNombre")
-let bSaludo = document.querySelector("#bSaludo")
-let bLegal = document.querySelector("#bLegal")
-let container = document.querySelector("#container")
+let Nombre = document.querySelector("#Nombre")
+let Edad = document.querySelector("#Edad")
+let bNombre =document.querySelector("#nombre")
+let bSaludo = document.querySelector("#saludo")
+let bLegal = document.querySelector("#legal")
+let hola = document.querySelector("#hola")
 
-
+let nombreAgregar = ''
+let edadAgregar = ''
 bNombre.addEventListener ('click', agregarNombre)
+bSaludo.addEventListener ('click', agregarSaludo)
+bLegal.addEventListener ('click', agregarLegal)
+
+
+
+Nombre.addEventListener ("input", (e) => {
+    nombreAgregar = e.target.value
+
+})
+Edad.addEventListener ("input", (e) => {
+    edadAgregar = e.target.value
+
+})
+
 
 function agregarNombre() {
-    if(nombre != null ) {
-        container.innerHTML = `<p>${nombre}</p>`
-        console.log(nombre);
+    if(nombreAgregar !=  "" ) {
+        hola.innerHTML = `<div class="justify-end">${nombreAgregar}</div>`
+
+    }
+}
+function agregarSaludo() {
+    if(edadAgregar !=  "" ) {
+        hola.innerHTML = `<div class="justify-end">${edadAgregar}</div>`
+
+    }
+}
+function agregarLegal() {
+    if(nombreAgregar !=  "" ) {
+        hola.innerHTML = `<div class="justify-end">${nombreAgregar}</div>`
 
     }
 }
